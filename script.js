22abcdcc1b87baf7af5abbc71dae7c6cab844f71
yoongi <lolhongy3@gmail.com>
2023-04-25 23:40:29 +1000

    refactor main code & add backspace function

--- script.js
+++ script.js
@@ -1,14 +1,71 @@
 const input = document.getElementById('input');
 const buttons = document.querySelectorAll('button');
 
-let prevNum;
-let currNum;
-let ans;
-let prevBtn;
-let operator;
+let firstOperand = '';
+let secondOperand = '';
+let currOperator = '';
+let ans = '';
 let shouldClear;
 
+buttons.forEach((button) => {
+    button.addEventListener('click', ()=>{
+        handleButtons(button);
+        console.log(button.classList+" "+ firstOperand+" "+currOperator+" "+secondOperand+" "+ans)
+    })
+});
+
+function handleButtons(button){
+    if(button.classList.contains('number')){
+        handleNumber(button.textContent);
+    }
+    if(button.classList.contains('operator')){
+        handleOperator(button.textContent);
+    }
+    if(button.classList.contains('equal')){
+        operate(currOperator, firstOperand, secondOperand);
+    }
+    if(button.classList.contains('clear')){
+        clear();
+    }
+    if(button.classList.contains('delete')){
+        backspace();
+    }
+    // decimal
+    // equal
+
+}
+
+function handleNumber(number){
+    if(input.textContent){
+        if(input.textContent[0].includes("0", 0) || shouldClear){
+            input.textContent = '';
+            shouldClear = false;
+        }
+        if(currOperator == ''){
+            firstOperand = '';
+        }
+    }
+    input.textContent += number;
+    secondOperand = input.textContent;
+}
+
+function handleOperator(operator){
+    if (firstOperand == ''){
+        firstOperand = secondOperand;
+        secondOperand = '';
+        input.textContent = '0';
+    }
+    if (!firstOperand == ''){
+        if(!secondOperand == ''){
+            operate(currOperator, firstOperand, secondOperand);
+        }
+    }
+    currOperator = operator;
+    return;
+}
+
 function operate(c, a, b){
+    (console.log(c))
     if (c === "+"){
         ans = (+a)+(+b);
     }if (c === "-"){
@@ -18,71 +75,22 @@ function operate(c, a, b){
     }if (c === "/"){
         ans = (+a)/(+b);
     }
-    console.log(ans)
     input.textContent = ans;
-    prevNum = '';
-    currNum = '';
-    operator = '';
+    firstOperand = ans;
+    ans = '';
+    secondOperand = '';
+    currOperator = '';
+    shouldClear = true;
 }
 
-buttons.forEach((button) => {
-    button.addEventListener('click', ()=>{
-        handleButtons(button);
-        console.log(button.classList+" "+ prevNum+" "+operator+" "+currNum+" "+ans)
-    })
-});
-
-function handleButtons(button){
-    if(button.classList.contains('number')){
-        if(input.textContent){
-            if(input.textContent[0].includes("0", 0) || shouldClear){
-                input.textContent = '';
-                shouldClear = false;
-            }
-        }
-        input.textContent += button.textContent;
-        currNum = input.textContent;
-    }
-    if(button.classList.contains('operator')){
-        if(prevBtn == "operator"){
-            operate(operator, prevNum, currNum);
-            operator = button.textContent;
-            prevNum = ans;
-            prevBtn = "operator";
-            shouldClear = true;
-            return;
-        }
-        if(prevBtn == "equal"){
-            operator = button.textContent;
-            prevNum = input.textContent;
-            input.textContent = "0";
-            prevBtn = "operator";
-            shouldClear = true;
-            return;
-        }
-        else
-        prevNum = currNum;
-        currNum = ''
-        operator = button.textContent;
-        input.textContent = "0";
-        prevBtn = "operator";
-    }
-    if(button.classList.contains('equal')){
-        prevBtn = "equal";
-        operate(operator, prevNum, currNum);
-    }
-    if(button.classList.contains('clear')){
-        prevBtn = "clear";
-        input.textContent = "0";
-        ans = '';
-        currNum = '';
-        prevNum = '';
-        operator = '';
-    }
-    // backspace
-    // decimal
-    // equal
-
+function clear(){
+    input.textContent = "0";
+    ans = '';
+    secondOperand = '';
+    firstOperand = '';
+    currOperator = '';
 }
-
-    
+function backspace(){
+    input.textContent = input.textContent.toString().slice(0,-1);
+    secondOperand = input.textContent;
+}
